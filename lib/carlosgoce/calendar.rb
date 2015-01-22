2d10c61b2f1d43fa98b46bdd17cfce86da95b7e5
Carlos Goce <carlosgoce@gmail.com>
2015-01-23 00:07:05 +0100

    Calendar can localize months

--- lib/carlosgoce/calendar.rb
+++ lib/carlosgoce/calendar.rb
@@ -1,7 +1,24 @@
+require 'rubygems'
+require 'active_support/core_ext/time'
+require 'active_support/core_ext/date'
+
 module CarlosGoce
   class Calendar
+    attr_reader :year
+
+    def initialize(year=Date.today.year)
+      @year = year
+    end
+
     def to_h
-      {enero: []}
+      h = {}
+      (1..12).each do |month|
+        month_name = I18n.t('date.month_names')[month].downcase
+        month_days =(1...Time.days_in_month(month, @year)).to_a.each_slice(7).to_a
+        h[month_name] = month_days
+      end
+
+      h
     end
   end
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,13 +1,27 @@
 require 'rspec'
+require 'rspec/expectations'
 require_relative '../lib/carlosgoce/calendar'
+
+I18n.load_path = Dir['config/locales/*.yml']
+I18n.backend.load_translations
 
 describe 'Calendar' do
   before(:each) do
     @calendar = CarlosGoce::Calendar.new
   end
 
+  it 'should be initialized with the current year' do
+    expect(@calendar.year).to eq(Date.today.year)
+  end
+
   describe 'Return months and days of given year' do
+    it 'should have all months of the year' do
+      expect(@calendar.to_h.count).to eq(12)
+    end
+
     it 'localize month names' do
+      expect(@calendar.to_h.keys.first.to_s).to eq('january')
+      I18n.locale = 'es'
       expect(@calendar.to_h.keys.first.to_s).to eq('enero')
     end
   end
