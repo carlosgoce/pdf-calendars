cf049dcdd8913bf79e5d33b41a5c5eda62d978e4
Carlos Goce <carlosgoce@gmail.com>
2015-01-23 20:49:38 +0100

    All specs fixed

--- lib/carlosgoce/layout/layouts.rb
+++ lib/carlosgoce/layout/layouts.rb
@@ -1,8 +1,13 @@
+require 'rubygems'
+require 'Prawn'
+
 module CarlosGoce
   module Layout
     class Simple
       def create (file, data, year)
-
+        Prawn::Document.generate(file) do
+          text 'Hello World'
+        end
       end
     end
   end
