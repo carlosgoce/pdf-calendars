be64f7b9962217177a18baf5c83b87cf6fbbaaab
Carlos Goce <carlosgoce@gmail.com>
2015-01-29 21:55:16 +0100

    Improved layout

--- lib/carlosgoce/layout/layouts.rb
+++ lib/carlosgoce/layout/layouts.rb
@@ -13,22 +13,19 @@ module CarlosGoce
           data[:months].each do |k, month|
             days = month[:formatted_days]
             cells = days.to_a.each_slice(7).to_a
-            cells.unshift %w(L M X J V S D)
-            cells.unshift [{content: "<color rgb='FF0000'><strong>#{month[:name]}</strong></color>", colspan: 7,
+            a = []
+            %w(L M X J V S D).each {|d| a << "<strong>#{d}</strong>" }
+            # cells.unshift ['L', 'M', 'X', 'J', 'V', 'S', 'D']
+            cells.unshift a
+            cells.unshift [{content: "<color rgb='FF0000'><strong>#{month[:name].upcase}</strong></color>", colspan: 7,
                            inline_format: true}]
 
             tables << make_table(cells,
-                                 cell_style:
-                                     {
-                                         width: 15, height: 12, align: :center, size: 7, border_width: 0,
-                                         padding: 0, valign: :center
-                                     })
+              cell_style: {width: 15, height: 12, align: :center, size: 7, border_width: 0, padding: 0, valign: :center})
           end
 
-          table(
-              tables.each_slice(3).to_a, cell_style: {align: :center, padding: [5, 5, 5, 5], border_width: 0,
-                                                      valign: :center}
-          )
+          table(tables.each_slice(3).to_a, cell_style: {align: :center, padding: [5, 5, 5, 5], border_width: 0,
+                                                      valign: :center, margin: [0, 0, 15, 0]})
         end
       end
     end
